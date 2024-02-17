6d2a45c78c488d55aa8f9f9a2667811efd2441f3
ehsan-0801 <ehsanulahmmed@gmail.com>
2024-02-17 22:40:42 +0600

    Updated

--- src/Components/PlaceSearch.jsx
+++ src/Components/PlaceSearch.jsx
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
-
+import { FaLocationDot } from "react-icons/fa6";
+import { PiAirplaneInFlight } from "react-icons/pi";
 const PlaceSearch = ({ LabelName, width }) => {
     const [place, setPlace] = useState("");
     const [suggestions, setSuggestions] = useState([]);
     const [focusedIndex, setFocusedIndex] = useState(-1);
+
+    const updateLabel = (suggestion) => {
+        const labelField = document.querySelector('.label_field');
+        if (suggestion.city_name || suggestion.airport_name) {
+            labelField.classList.add('filled');
+        } else {
+            labelField.classList.remove('filled');
+        }
+    };
 
     const handleKeyDown = (e) => {
         if (e.key === 'ArrowDown') {
@@ -14,15 +24,13 @@ const PlaceSearch = ({ LabelName, width }) => {
             setFocusedIndex(prevIndex => (prevIndex - 1 + suggestions.length) % suggestions.length);
         } else if (e.key === 'Enter' && focusedIndex !== -1) {
             e.preventDefault();
-            const placeName = suggestions[focusedIndex].city_name + ", " + suggestions[focusedIndex].country_name + "(" + suggestions[focusedIndex].code + ")"
-            setPlace(placeName);
+            const selectedSuggestion = suggestions[focusedIndex];
+            const placeName = selectedSuggestion.city_name + ", " + selectedSuggestion.country_name + "(" + selectedSuggestion.code + ")";
+            const airportName = selectedSuggestion.airport_name + ", " + selectedSuggestion.country_name + "(" + selectedSuggestion.code + ")";
+            setPlace(placeName, airportName);
             setSuggestions([]);
             setFocusedIndex(-1);
-            if (suggestions[focusedIndex].city_name || suggestions[focusedIndex].airport_name) {
-                document.querySelector('.label_field').classList.add('filled');
-            } else {
-                document.querySelector('.label_field').classList.remove('filled');
-            } // Reset focused index
+            updateLabel(selectedSuggestion);
         }
     };
 
@@ -43,10 +51,10 @@ const PlaceSearch = ({ LabelName, width }) => {
             const data = await response.json();
             const filteredSuggestions = data.filter(item =>
                 (item.city_name && item.city_name.toLowerCase().includes(value.toLowerCase())) ||
-                (item.country_name && item.country_name.toLowerCase().includes(value.toLowerCase()))
-                ||
+                (item.country_name && item.country_name.toLowerCase().includes(value.toLowerCase())) ||
                 (item.airport_name && item.airport_name.toLowerCase().includes(value.toLowerCase()))
             );
+            // console.log(filteredSuggestions)
             setSuggestions(filteredSuggestions);
         } catch (error) {
             console.error('There was a problem with the fetch operation:', error.message);
@@ -54,15 +62,11 @@ const PlaceSearch = ({ LabelName, width }) => {
     };
 
     const handleSuggestionClick = (suggestion) => {
-        const placeName = suggestion.city_name + ", " + suggestion.country_name + "(" + suggestion.code + ")"
+        const placeName = suggestion.city_name + ", " + suggestion.country_name + "(" + suggestion.code + ")";
         setPlace(placeName);
         setSuggestions([]);
         setFocusedIndex(-1);
-        if (suggestion.city_name || suggestion.airport_name) {
-            document.querySelector('.label_field').classList.add('filled');
-        } else {
-            document.querySelector('.label_field').classList.remove('filled');
-        }
+        updateLabel(suggestion);
     };
     return (
         <div className='relative'>
@@ -74,15 +78,24 @@ const PlaceSearch = ({ LabelName, width }) => {
                 onChange={ (e) => handlePlaceChange(e.target.value) }
                 onKeyDown={ handleKeyDown }
             />
-            <label htmlFor="" className='absolute top-3 left-2 label_field'>{ LabelName }</label>
+            <label className='absolute top-3 left-2 label_field'>{ LabelName }</label>
             <ul className="absolute z-10 bg-white border border-gray-300 rounded mt-1 w-[300px] shadow-md max-h-48 overflow-y-auto w-[400px]">
                 { suggestions.map((suggestion, index) => (
                     <li key={ index }
                         className={ `Suggestion_list cursor-pointer py-1 px-2 hover:bg-gray-100 ${index === focusedIndex ? 'focused' : ''}` }
                         onClick={ () => handleSuggestionClick(suggestion) }>
                         <div className='flex items-center justify-between'>
-                            <p>{ suggestion.city_name || suggestion.airport_name }, { suggestion.country_name }</p>
-
+                            <p className='flex items-center justify-center'> <FaLocationDot color='#188920' className='mr-2' />{ suggestion.city_name }, { suggestion.country_name }</p>
+                            <p>{ suggestion.code }</p>
+                        </div>
+                    </li>
+                )) }
+                { suggestions.map((suggestion, index) => (
+                    <li key={ index }
+                        className={ `Suggestion_list cursor-pointer py-1 px-2 hover:bg-gray-100 ${index === focusedIndex ? 'focused' : ''}` }
+                        onClick={ () => handleSuggestionClick(suggestion) }>
+                        <div className='flex items-center justify-between'>
+                            <p className='flex items-center justify-center'> <PiAirplaneInFlight color='#188920' className='mr-2' />{ suggestion.airport_name }, { suggestion.country_name }</p>
                             <p>{ suggestion.code }</p>
                         </div>
                     </li>
